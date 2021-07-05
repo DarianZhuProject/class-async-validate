f0ea864dd0ffdcda6cdb11d483ec2c56e5b5af9f
zhuzhengjian <aibyanyu@qq.com>
2021-07-05 15:22:45 +0800

    修改了打包的方式支持egg

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import BaseValidate from "@/lib/validate";
-import { ValidateRules,DataParams,SceneItem,ConfigOption,CallbackData,Scenes } from "@/lib/typing"
+import BaseValidate from "./lib/validate";
+import { ValidateRules,DataParams,SceneItem,ConfigOption,CallbackData,Scenes } from "./lib/typing"
 export {
     BaseValidate,
     ValidateRules,
